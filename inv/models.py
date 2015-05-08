a3e4a9196c8b3eb0bce2baca3e6e6ee79b415b45
Juan Lautaro Silva Ortiz <jlautarosilva@gmail.com>
2015-05-08 06:49:09 -0300

    doc

--- inv/models.py
+++ inv/models.py
@@ -6,24 +6,24 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Inversion(models.Model):
 
     num_corr = models.AutoField(primary_key=True)
-    ano_postul = models.IntegerField("in_value=1, max_value=9999",null=True, blank=True)
-    situacion = models.IntegerField("in_value=0, max_value=9",null=True, blank=True)
-    id_bip_n = models.IntegerField("in_value=10000000, max_value=99999999",null=True, blank=True)
+    ano_postul = models.IntegerField("Ano postulacion",null=True, blank=True) #4
+    situacion = models.IntegerField("Situacion",null=True, blank=True) # 0 a 9
+    id_bip_n = models.IntegerField("Codigo BIP numero",null=True, blank=True) #8
     id_bip_t = models.CharField('Codigo BIP texto',max_length=8,null=True, blank=True)
     x = models.DecimalField(max_digits = 6, decimal_places = 3,null=True, blank=True)
     y = models.DecimalField(max_digits = 7, decimal_places = 6,null=True, blank=True)
-    obs1 = models.IntegerField("in_value=0, max_value=99",null=True, blank=True)
-    cod_comu = models.IntegerField("in_value=0, max_value=9999",null=True, blank=True)
-    cod_corr = models.IntegerField(" max 5",null=True, blank=True)
-    cod_estado = models.IntegerField("max 2",null=True, blank=True)
+    obs1 = models.IntegerField("OBS 1",null=True, blank=True) # 2
+    cod_comu = models.AutoField("Codigo comuna",null=True, blank=True) #4
+    cod_corr = models.IntegerField("Codigo Corr",null=True, blank=True) #5
+    cod_estado = models.IntegerField("Codigo Estado",null=True, blank=True) #2
     estado_postu = models.CharField('Estado de la Postulacion', max_length=18,null=True, blank=True)
-    cod_proy_ugit = models.IntegerField('max 18',null=True, blank=True)
+    cod_proy_ugit = models.IntegerField('Codigo Proyecto UGIT',null=True, blank=True) #18
     rep_conadi = models.CharField('Rep CONADI', max_length=9,null=True, blank=True)
     nom_proy = models.CharField('Nombre del Proyecto',max_length=999,null=True, blank=True)
-    n_cert_core = models.CharField('abc', max_length=4,null=True, blank=True)
-    n_ses_core = models.IntegerField('max4',null=True, blank=True)
+    n_cert_core = models.CharField('Numero certificado CORE', max_length=4,null=True, blank=True) #abc
+    n_ses_core = models.IntegerField('Numero sersion CORE',null=True, blank=True) #4
     fec_core = models.DateField(null=True, blank=True)
-    mont_pesos = models.IntegerField('max 12',null=True, blank=True)
+    mont_pesos = models.IntegerField('Monto pesos',null=True, blank=True) #12
     fuen_finan = models.CharField(max_length=30,null=True, blank=True)
     comuna = models.CharField(max_length=20,null=True, blank=True)
     provincia = models.CharField(max_length=10,null=True, blank=True)
